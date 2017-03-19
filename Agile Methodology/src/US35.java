b733914a6c3714f5bf8fd992bca2cc96ef886e77
Palak Gangwal <Palak Gangwal@LAPTOP-K4TFCBJQ.fios-router.home>
2017-03-19 19:07:11 -0400

    US35 recent births

---
+++ Agile Methodology/src/US35.java
@@ -0,0 +1,56 @@
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+//import java.util.Calendar;
+import java.util.Date;
+//import java.util.GregorianCalendar;
+import java.util.Locale;
+
+public class US35 {
+	
+	static Connection con = null;
+	static Statement stmt = null;
+	static String lineSeparator = "======================================================================="
+			+ "============================================================================\n";
+	
+	public static void RecentBirths() throws SQLException, ParseException {
+
+		System.out.println(lineSeparator + "\n****Start of US35****\n");
+
+		String IndiName = "";
+		String birthday = "";
+		
+		int age = 0;
+
+		con = JDBCConnect.getConnection();
+		stmt = con.createStatement();
+//
+		String query = "select i.name, to_date(NULLIF(i.birthday,''), 'DD Mon YYYY') from individuals i where invalidbirthrecord='N'  ";
+		
+		ResultSet rs = stmt.executeQuery(query);
+		DateFormat format = new SimpleDateFormat("yyyy-MM-dd", Locale.ENGLISH);
+		while (rs.next()) {
+		IndiName = rs.getString(1); // Listing all individual's names
+		birthday = rs.getString(2);// birthday
+		String todaysDate =  java.time.LocalDate.now().toString();
+		if(birthday!=null){
+		Date todayDate  = new Date();
+		todayDate = format.parse(todaysDate); //  parsing to proper format
+		
+		Date bDate  = new Date();
+		bDate = format.parse(birthday);//  parsing to proper format
+		int Recent = DatesCalc.getDiff(bDate.toString() ,todayDate.toString() ,DatesCalc.DAY); 
+	
+		if(Recent< 30)// display individual name is born in lst 30 days
+		System.out.println(lineSeparator  +"Individuals  "+ IndiName + "is born in last 30 days");
+	  
+	}	
+		}
+              // else display no indivauls
+		 System.out.println(lineSeparator + "ERROR:\tINDIVIDUAL:\tUS27:\t" +"No individual was born in last 30 days");
+	}	// method to calculate the diffrence in days (recent birth >= 30 days)
+}
